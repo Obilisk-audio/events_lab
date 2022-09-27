8abaf737d237a6bf3127e0747c2a914679b3f7a1
Milo Curran <milo.curran@icloud.com>
2022-09-27 13:23:25 +0100

    first push

---
+++ code/lab_jsevents_start_code/script.js
@@ -0,0 +1,5 @@
+const paragraphs = document.querySelectorAll(".toDo");
+console.log(paragraphs);
+
+paragraphs[0].innerText = "The DOM is a hierarchal representation of the HTML elements in your document";
+paragraphs[1].innerText = "It provides us with an API which allows us to traverse our HTML document securely";
