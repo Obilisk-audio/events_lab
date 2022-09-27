9a852486916296e3d69a9d73d16fbbaa3c2963a4
Yongran <maiyongran@gmail.com>
2022-09-27 14:27:11 +0100

    refactor

--- code/lab_jsevents_start_code/script.js
+++ code/lab_jsevents_start_code/script.js
@@ -1,23 +1,27 @@
 const enter = document.querySelector("#enter");
 const input = document.querySelector("#new-todo");
 const list = document.querySelector("#list");
-const deleteBtn = document.querySelector("#delete");
 
-enter.addEventListener('click', () => {
+const deleteFun = function (){
+    let listItem = this.parentNode;
+    let curList = listItem.parentNode;
+    curList.removeChild(listItem);
+}
+
+const addFun = function (){
     const newInput = input.value;
     const listItem = document.createElement("li");
+    listItem.innerText = newInput;
+
     const deleteBtn = document.createElement("button");
     deleteBtn.id="delete";
     const deleteBtnText = document.createTextNode("Delete");
     deleteBtn.appendChild(deleteBtnText);
-    listItem.innerText = newInput;
+
     listItem.appendChild(deleteBtn);
     list.appendChild(listItem);
-    
-    deleteBtn.addEventListener('click',()=>{
-    
-        list.removeChild(listItem);
-    
-    })
 
-})
+    deleteBtn.addEventListener('click', deleteFun);
+}
+
+enter.addEventListener('click', addFun);
