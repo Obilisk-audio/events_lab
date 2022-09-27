3e42b92dc4128657a653ce27b15ba49f1d0ee295
Vilte N <viltenor@gmail.com>
2022-09-27 16:03:15 +0300

    created delete button

--- code/lab_jsevents_start_code/script.js
+++ code/lab_jsevents_start_code/script.js
@@ -1,14 +1,23 @@
 const enter = document.querySelector("#enter");
 const input = document.querySelector("#new-todo");
 const list = document.querySelector("#list");
+const deleteBtn = document.querySelector("#delete");
 
 enter.addEventListener('click', () => {
     const newInput = input.value;
     const listItem = document.createElement("li");
     const deleteBtn = document.createElement("button");
+    deleteBtn.id="delete";
     const deleteBtnText = document.createTextNode("Delete");
     deleteBtn.appendChild(deleteBtnText);
     listItem.innerText = newInput;
+    listItem.appendChild(deleteBtn);
     list.appendChild(listItem);
-    list.appendChild(deleteBtn);
+    
+    deleteBtn.addEventListener('click',()=>{
+    
+        list.removeChild(listItem);
+    
+    })
+
 })
